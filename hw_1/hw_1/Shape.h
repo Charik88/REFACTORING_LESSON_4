605d4a035679f1e59ffe04718a346129a4a48b71
Anastasia <A.charykova@list.ru>
2024-12-07 17:41:09 +0300

    FIRST COMMIT

---
+++ hw_1/hw_1/Shape.h
@@ -0,0 +1,36 @@
+//
+//  Shape.h
+//  hw_1
+//
+//  Created by Anastasiya Charykova on 07.12.2024.
+//
+
+#pragma once
+#include<string>
+class Shape
+{
+public:
+    static const int line = 0; // тип линии
+    static const int sqr = 1; // тип квадрата
+    static const int cube = 2; // тип куба
+    static const int circle = 3; // тип окружности
+    static const int cylinder = 4; // тип цилиндра
+    Shape() = default; // конструктор по умолчанию
+    Shape(int type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8); // конструктор класса Shape
+    Shape(int type, int _x1, int _y1, double R, double H); //
+    int getType() { return type; } // получение типа фигуры
+
+    int type; // тип фигуры
+    int x1 = 0, y1 = 0, z1 = 0,
+        x2 = 0, y2 = 0, z2 = 0,
+        x3 = 0, y3 = 0, z3 = 0,
+        x4 = 0, y4 = 0, z4 = 0,
+        x5 = 0, y5 = 0, z5 = 0,
+        x6 = 0, y6 = 0, z6 = 0,
+        x7 = 0, y7 = 0, z7 = 0,
+        x8 = 0, y8 = 0, z8 = 0;
+    double volume; // обьем фигуры
+    double square; // площадь фигуры
+    double height; // высота цилиндра
+    double radius; // радиус окружности
+};
